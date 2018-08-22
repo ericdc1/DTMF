17a4678f9878d90e67d061a221c8a5d210dd33dd
Peter Santiago <psantiago@hsc.wvu.edu>
2018-08-22 14:28:58 -0400

    fixed service worker to not be terrible - it'll now self-update/self-install, and not call things multiple times

--- Source/DTMF.Website/sw.js
+++ Source/DTMF.Website/sw.js
@@ -1,13 +1,18 @@
 ﻿//This is the "Offline copy of pages" service worker
+var CACHE_NAME = "dtmf-cache";
+
 
 //Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener('install', function (event) {
+    //rather than gracefully waiting for the tab to die, just push the old one down a pit and use this one instead
+    //https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle#skip_the_waiting_phase
+    self.skipWaiting();
+
     var indexPage = new Request('/');
     event.waitUntil(
         fetch(indexPage).then(function (response) {
-            return caches.open('pwabuilder-offline').then(function (cache) {
-                //debugger;
-                console.log('[PWA Builder] Cached index page during Install' + response.url);
+            return caches.open(CACHE_NAME).then(function(cache) {
+                console.log('SW - cached index page during Install' + response.url);
                 return cache.put(indexPage, response);
             });
         }));
@@ -15,33 +20,36 @@ self.addEventListener('install', function (event) {
 
 //If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener('fetch', function (event) {
-    //debugger;
-    var updateCache = function (request) {
-        return caches.open('pwabuilder-offline').then(function (cache) {
-            return fetch(request).then(function (response) {
-                console.log('[PWA Builder] add page to offline' + response.url);
-                return cache.put(request, response);
+
+    event.respondWith(fetch(event.request).then(function(response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                cache.put(event.request, responseToCache);
+            });
+
+        return response;
+    }).catch(function (error) {
+        console.log('SW - Network request Failed. Serving content from cache: ' + error);
+
+        //Check to see if you have it in the cache
+        //Return response
+        //If not in the cache, then return error page
+        return caches.open(CACHE_NAME).then(function (cache) {
+            return cache.match(event.request).then(function (matching) {
+                var report = !matching || matching.status == 404 ? Promise.reject('no-match') : matching;
+                return report;
             });
         });
-    };
-
-    event.waitUntil(updateCache(event.request));
-
-    event.respondWith(fetch(event.request)
-        .catch(function (error) {
-            console.log('[PWA Builder] Network request Failed. Serving content from cache: ' + error);
-
-            //Check to see if you have it in the cache
-            //Return response
-            //If not in the cache, then return error page
-            return caches.open('pwabuilder-offline').then(function (cache) {
-                //debugger;
-                return cache.match(event.request).then(function (matching) {
-                    debugger;
-                    var report = !matching || matching.status == 404 ? Promise.reject('no-match') : matching;
-                    return report;
-                });
-            });
-        })
-    );
+    }));
 });
