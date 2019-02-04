58be4c95adb1e1984387ba50b8f20aee933fe096
Peter Santiago <psantiago@hsc.wvu.edu>
2019-02-04 15:20:22 -0500

    support fancy tls k thnx bai

--- Source/DTMF.Website/Global.asax.cs
+++ Source/DTMF.Website/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Net;
+using System.Security.Claims;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -10,6 +11,8 @@ namespace DTMF
     {
         protected void Application_Start()
         {
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
